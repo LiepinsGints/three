25b0581b290ac5d19fc846b15aeb13f1d6610fc0
LiepinsDuPatstavigais <liepins.gints@jak.lv>
2015-01-26 19:38:10 +0200

    Segment And vertice detection

--- js/label.js
+++ js/label.js
@@ -5,6 +5,9 @@ function updateLabel(delta){
 document.getElementById("CamX").innerHTML=Math.round(camera.position.x);
 document.getElementById("CamY").innerHTML=Math.round(camera.position.y);
 document.getElementById("CamZ").innerHTML=Math.round(camera.position.z);
+document.getElementById("PlaneVertices").innerHTML=plane.geometry.vertices.length;
+document.getElementById("CurentSegment").innerHTML=planePos.getSegment(planeW, planeH, planeWs, planeHs,cube.position.x,cube.position.y);
+document.getElementById("LeftVertice").innerHTML=planePos.getVertices(planeW, planeH, planeWs, planeHs,cube.position.x,cube.position.y);
 
 
 
---
+++ js/planePos.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+//insert after three js
+function PlanePos(){
+
+
+this.getSegment=function getSegment (planeW, planeH, planeWs, planeHs,coordX, coordY) {
+//<-- cordinate diapazons
+
+this.DiapX=planeW/2;
+this.DiapY=planeH/2;
+//<-- 
+if((coordX>=-1*this.DiapX && coordX<this.DiapX)&& (coordY>=-1*this.DiapY && coordY<this.DiapY)){
+this.DiapX=this.DiapX+coordX;
+this.DiapY=this.DiapY-+coordY;
+
+//<-- segment dimanesions
+this.SegmentWidth=planeW/planeWs;
+this.SegmentHeight=planeH/planeHs;
+//<--
+this.SegX=Math.ceil(this.DiapX/this.SegmentWidth);
+this.SegY=Math.ceil(this.DiapY/this.SegmentHeight);
+
+//return(this.SegX*planeWs-(planeHs-this.SegY));
+return (this.SegY*planeWs-(planeHs-this.SegX));
+}
+return(0);
+}//<--
+
+this.getVertices=function getVertices(planeW, planeH, planeWs, planeHs,coordX, CoordY){
+this.segment= this.getSegment (planeW, planeH, planeWs, planeHs,coordX, CoordY);
+//<-- first vertice number
+return(this.segment+(Math.ceil(this.segment/planeWs)-1)); 	
+	
+}//<--
+
+	
+	
+	
+	
+	
+}//<-- plane
